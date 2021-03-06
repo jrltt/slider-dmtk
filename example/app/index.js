c6313d7e94662568246a5c505e0e79dc310926c5
Joaquín <hola@jrltt.net>
2021-03-06 10:49:50 +0100

    Basic implementation

---
+++ example/app/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+// TODO replace for package from node_modules
+import { Slider } from './../../dist/main';
+
+const e = new Slider();
+e.init({ elementName: '.slider' });
---
+++ example/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'app'),
+    port: 3500,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: './app/index.html' }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
